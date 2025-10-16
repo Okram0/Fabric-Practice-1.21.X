1926d22af31b0b47212523ebce54cc7ef9da4583
Panovich <panovichma25@gcc.edu>
2025-10-16 14:31:59 -0400

    Initial commit

---
+++ src/main/java/net/okram/practicemod/PracticeModClient.java
@@ -0,0 +1,10 @@
+package net.okram.practicemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PracticeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

e39d4244d30c6cbe3c7df6f127a80e8b3385dc4a
Okram <panovichmarko@gmail.com>
2025-11-01 16:34:32 -0400

    Adds item and item framework

--- src/main/java/net/okram/practicemod/PracticeMod.java
+++ src/main/java/net/okram/practicemod/PracticeMod.java
@@ -2,6 +2,7 @@ package net.okram.practicemod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.okram.practicemod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,6 @@ public class PracticeMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/okram/practicemod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.okram.practicemod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.okram.practicemod.PracticeMod;
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(PracticeMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        PracticeMod.LOGGER.info("Registering Mod Items For "+PracticeMod.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+    }
+}
